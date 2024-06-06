4cd95cc46aacd54c93a24691a0e072971865ba7a
Isabella Lois <lestariayu0801@gmail.com>
2024-06-06 07:05:12 +0700

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-// module.exports = {
-//   images:{
-//     domains:["res.cloudinary.com"]
-//   }
-// }
-
 module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  reactStrictMode: true, // Tambahkan ini untuk mengaktifkan mode ketat React
 };
